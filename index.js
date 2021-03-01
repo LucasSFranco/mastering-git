1f2991e640028ec2dc358f861b32985a3337a45e
LucasSFranco <surdifranco@gmail.com>
2021-03-01 10:49:28 -0300

    added counter logic on button hold

--- index.js
+++ index.js
@@ -17,6 +17,40 @@ function decrement() {
 incrementButton.addEventListener("click", increment)
 decrementButton.addEventListener("click", decrement)
 
+let incrementTimeout = null
+let incrementInterval = null
+incrementButton.addEventListener("mousedown", () => {
+  incrementTimeout = setTimeout(() => {
+    incrementInterval = setInterval(increment, 100) 
+  }, 250)
+})
+
+incrementButton.addEventListener("mouseup", () => {
+  clearTimeout(incrementTimeout)
+  clearInterval(incrementInterval)
+})
+incrementButton.addEventListener("mouseout", () => {
+  clearTimeout(incrementTimeout)
+  clearInterval(incrementInterval)
+})
+
+let decrementTimeout = null
+let decrementInterval = null
+decrementButton.addEventListener("mousedown", () => {
+  decrementTimeout = setTimeout(() => {
+    decrementInterval = setInterval(decrement, 100) 
+  }, 250)
+})
+
+decrementButton.addEventListener("mouseup", () => {
+  clearTimeout(decrementTimeout)
+  clearInterval(decrementInterval)
+})
+decrementButton.addEventListener("mouseout", () => {
+  clearTimeout(decrementTimeout)
+  clearInterval(decrementInterval)
+})
+
 function update() {
   viewer.textContent = counter
 }
